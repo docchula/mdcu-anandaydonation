f0265ca9762c79a509f900a60a1a8a084d02e0c5
Bebiezaza <Apipunnakul@gmail.com>
2026-03-05 12:40:57 +0700

    feat: full listing statistics between all and confirmed

--- src/app/components/StatisticalData.tsx
+++ src/app/components/StatisticalData.tsx
@@ -5,6 +5,7 @@ import React from "react";
 interface StatisticalDataProps {
   statistics: {
     totalDonators: number;
+    totalDonatorsConfirmed: number;
     totalMoney: number;
     totalMoneyApproved: number;
     totalCardwithboxOrders: number;
@@ -13,6 +14,10 @@ interface StatisticalDataProps {
     donatorsWithShirtOrder: number;
     donatorsWithCardOrder: number;
     donatorsWithBothOrders: number;
+    donatorsWithoutOrderConfirmed: number;
+    donatorsWithShirtOrderConfirmed: number;
+    donatorsWithCardOrderConfirmed: number;
+    donatorsWithBothOrdersConfirmed: number;
     totalCardOrders: number;
     totalCardOrdersApproved: number;
     totalShirtOrders: { [key: string]: number };
@@ -24,29 +29,23 @@ const StatisticalData: React.FC<StatisticalDataProps> = ({ statistics }) => {
   return (
     <div className="space-x-4 space-y-0.5">
       <h2 className="text-xl font-bold">ข้อมูลสถิติ</h2>
-      <p>ผู้บริจาครวมทั้งหมด: {statistics.totalDonators}</p>
-      <p>ยอดบริจาครวม (ทั้งหมด): {statistics.totalMoney}</p>
-      <p>ยอดบริจาครวม (ตรวจสอบการชำระเงินแล้ว): {statistics.totalMoneyApproved}</p>
+      <p>ผู้บริจาครวมทั้งหมด (ยืนยันการชำระเงินแล้ว): {statistics.totalDonatorsConfirmed}</p>
+      <p>ยอดบริจาครวม (ยืนยันการชำระเงินแล้ว): {statistics.totalMoneyApproved}</p>
+      <p>ผู้บริจาคที่ไม่มีคำสั่งซื้อ (ยืนยันการชำระเงินแล้ว): {statistics.donatorsWithoutOrderConfirmed}</p>
+      <p>ผู้บริจาคที่สั่งซื้อเสื้อที่ระลึก (ยืนยันการชำระเงินแล้ว): {statistics.donatorsWithShirtOrderConfirmed}</p>
+      <p>ผู้บริจาคที่สั่งซื้อเข็มที่ระลึกหรือเข็มพร้อมกล่อง (ยืนยันการชำระเงินแล้ว): {statistics.donatorsWithCardOrderConfirmed}</p>
+      <p>ผู้บริจาคที่สั่งซื้อทั้งเสื้อที่ระลึกและเข็มที่ระลึก (ยืนยันการชำระเงินแล้ว): {statistics.donatorsWithBothOrdersConfirmed}</p>
+      <p>ยอดสั่งซื้อเข็มที่ระลึกทั้งหมด (ยืนยันการชำระเงินแล้ว): {statistics.totalCardOrdersApproved}</p>
+      <p>ยอดสั่งซื้อเข็มพร้อมกล่องทั้งหมด (ยืนยันการชำระเงินแล้ว): {statistics.totalCardwithboxOrdersApproved}</p>
+      <br />
+      <p>ผู้บริจาครวมทั้งหมด (รวมปฏิเสธและรอดำเนินการ): {statistics.totalDonators}</p>
+      <p>ยอดบริจาครวม (ทั้งหมด รวมปฏิเสธและรอดำเนินการ): {statistics.totalMoney}</p>
       <p>ผู้บริจาคที่ไม่มีคำสั่งซื้อ: {statistics.donatorsWithoutOrder}</p>
       <p>ผู้บริจาคที่สั่งซื้อเสื้อที่ระลึก: {statistics.donatorsWithShirtOrder}</p>
       <p>ผู้บริจาคที่สั่งซื้อเข็มที่ระลึกหรือเข็มพร้อมกล่อง: {statistics.donatorsWithCardOrder}</p>
-      <p>
-        ผู้บริจาคที่สั่งซื้อทั้งเสื้อที่ระลึกและเข็มที่ระลึก:{" "}
-        {statistics.donatorsWithBothOrders}
-      </p>
+      <p>ผู้บริจาคที่สั่งซื้อทั้งเสื้อที่ระลึกและเข็มที่ระลึก: {statistics.donatorsWithBothOrders}</p>
       <p>ยอดสั่งซื้อเข็มที่ระลึกทั้งหมด: {statistics.totalCardOrders}</p>
-      <p>
-        ยอดสั่งซื้อเข็มที่ระลึกทั้งหมด (ตรวจสอบการชำระเงินแล้ว):{" "}
-        {statistics.totalCardOrdersApproved}
-      </p>
-      <p>
-        ยอดสั่งซื้อเข็มพร้อมกล่องทั้งหมด:{" "}
-        {statistics.totalCardwithboxOrders}
-      </p>
-      <p>
-        ยอดสั่งซื้อเข็มพร้อมกล่องทั้งหมด (ยืนยันการชำระเงินแล้ว):{" "}
-        {statistics.totalCardwithboxOrdersApproved}
-      </p>
+      <p>ยอดสั่งซื้อเข็มพร้อมกล่องทั้งหมด: {statistics.totalCardwithboxOrders}</p>
       <h3 className="font-bold">ยอดสั่งซื้อเสื้อที่ระลึกทั้งหมด:</h3>
       {Object.entries(statistics.totalShirtOrders).map(([key, count]) => (
         <p key={key}>
